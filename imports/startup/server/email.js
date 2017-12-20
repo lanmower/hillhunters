e7682bcbd340bbe605d9008cb7772527bc00eb9a
James <almagestfraternite@gmail.com>
2017-12-20 14:32:45 +0200

    settings fix

--- imports/startup/server/email.js
+++ imports/startup/server/email.js
@@ -1,3 +1,3 @@
 import { Meteor } from 'meteor/meteor';
 
-if (Meteor.isDevelopment) process.env.MAIL_URL = Meteor.settings.private.MAIL_URL;
+if (Meteor.isDevelopment && Meteor.settings.private) process.env.MAIL_URL = Meteor.settings.private.MAIL_URL;
